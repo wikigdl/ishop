6dd65fea995235de9b595f4c0aafe35429b0bb30
Sergio Molina <wikigdl@gmail.com>
2012-08-02 09:38:35 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ishop::Application.config.secret_token = 'd1b5da923191aaa599272b3d766169193857aec5afa4cf3fa21cc1fe48d7ecdeb6f00336afaa26fd1b31b92673d4825abc78671bae3d5bd5e5ad140220f73e88'
